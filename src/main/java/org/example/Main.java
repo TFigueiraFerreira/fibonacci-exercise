c8ff5cce310b42b20730e7244dd1ce042afc9843
Tiago Ferreira <tfigueiraferreira@gmail.com>
2024-10-16 12:27:48 +0100

    Explicação

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,9 +7,11 @@ public class Main {
     }
 
     public static int F(int n) {
+        // Caso base, se n for 0 ou 1, retorna o próprio valor de n.
         if (n <= 1) {
             return n;
         }
+        //Soma o resultado da função F
         return F(n - 1) + F(n - 2);
     }
 }
